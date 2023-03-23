35f3dc5ed4705c38ef706fbffba156e84418310a
Roberto Fernandes <robertofernandes199@hotmail.com>
2023-03-23 18:25:47 -0300

    config .env

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV == "production") {
-//     module.exports { mongoURI: " mongodb+srv://robertofernandes1:snS58n0bIFRdDFFn@cluster0.prmm3ox.mongodb.net/?retryWrites=true&w=majority" }
+if (process.env.NODE_ENV == "production") {
+    module.exports = { mongoURI: "NODE_ENV" }
     
-// } else {
-//     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
-// }
+} else {
+    module.exports = {mongoURI: "mongodb://localhost/blogapp"}
+}
